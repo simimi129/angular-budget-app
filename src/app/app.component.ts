c16eb2aff44301efa306315ed15848e7f704b20d
simimi129 <simimi129@gmail.com>
2023-01-24 21:30:33 +0100

    adding design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-budget-app';
+  isNotifOn: boolean = false;
+  isUserOn: boolean = false;
+
+  turnNotifOn() {
+    this.isNotifOn = true;
+  }
+
+  turnNotifOff() {
+    this.isNotifOn = false;
+  }
+
+  turnUserOn() {
+    this.isUserOn = true;
+  }
+
+  turnUserOff() {
+    this.isUserOn = false;
+  }
 }
