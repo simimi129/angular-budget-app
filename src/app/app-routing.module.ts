788dd8dc553b142476a63466c8cc9ef1074edca3
simimi129 <simimi129@gmail.com>
2023-01-25 15:48:25 +0100

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ManageComponent } from './manage/manage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+  { path: 'manage', component: ManageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, OverviewComponent, AnalyticsComponent, ManageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
