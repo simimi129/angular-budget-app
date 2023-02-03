ec5fe5c0b4b3193cd5ad2896fd2fbc723846d10d
simimi129 <simimi129@gmail.com>
2023-02-03 12:29:23 +0100

    adding transactions service

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Transaction,
+  TransactionsService,
+} from '../services/transactions.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
+  styleUrls: ['./overview.component.css'],
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  public transactions$: Observable<Transaction[]> | undefined;
+  public income$: Observable<number> | undefined;
+  public outcome$: Observable<number> | undefined;
 
+  constructor(private transactionsService: TransactionsService) {}
+
+  ngOnInit(): void {
+    this.transactions$ = this.transactionsService.getTransactions();
+    console.log(this.transactions$);
+    this.income$ = this.transactionsService.getIncome();
+    this.outcome$ = this.transactionsService.getOutcome();
+
+    this.transactionsService.init();
+  }
 }
--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -5,11 +5,15 @@ import { HttpClient } from '@angular/common/http';
 export interface Transaction {
   name: string;
   amount: number;
+  category: string;
+  type: string;
 }
 
 @Injectable({ providedIn: 'root' })
 export class TransactionsService {
   private transactions$ = new BehaviorSubject<Transaction[]>([]);
+  private income$ = new BehaviorSubject<number>(0);
+  private outcome$ = new BehaviorSubject<number>(0);
 
   constructor(private http: HttpClient) {}
 
@@ -18,12 +22,30 @@ export class TransactionsService {
       .get<Transaction[]>(
         'https://ng-budget-b59c9-default-rtdb.europe-west1.firebasedatabase.app/transactions.json'
       )
-      .subscribe((transactions) => {
-        this.transactions$.next(Object.values(transactions));
+      .subscribe((data) => {
+        const transactions = Object.values(data);
+        const income = transactions
+          .filter((t) => t.type === 'income')
+          .reduce((sum, t2) => sum + t2.amount, 0);
+        const outcome = transactions
+          .filter((t) => t.type === 'outcome')
+          .reduce((sum, t2) => sum + t2.amount, 0);
+        this.transactions$.next(transactions);
+        this.income$.next(income);
+        this.outcome$.next(outcome);
+        console.log(this.transactions$);
       });
   }
 
   public getTransactions(): Observable<Transaction[]> {
     return this.transactions$;
+  }
+
+  public getIncome(): Observable<number> {
+    return this.income$;
+  }
+
+  public getOutcome(): Observable<number> {
+    return this.outcome$;
   }
 }
